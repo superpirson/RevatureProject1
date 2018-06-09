ff2cb3ca066e32e3a7ea5515e15d69a3cbb5b860
rafaeltx <rafb.mariano@gmail.com>
2018-06-09 15:39:54 -0400

    adding database sql

---
+++ RevatureProject1.sql
@@ -0,0 +1,5 @@
+create table account_table(account_id varchar(13), username varchar(13), pass varchar(13), fName varchar(13), lName varchar(13), account_type varchar(13), 
+    reportsto varchar(13), primary key(account_id));
+    
+    
+create table forms_table(id number, fName varchar(13), lName varchar(13), grade number, date_compeleted varchar(13), employee_approval varchar(13));
