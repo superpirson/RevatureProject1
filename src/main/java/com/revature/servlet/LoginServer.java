312782b3555fc4d677d239dec8b90eacfd393f4d
Alex Patton <superpirson@gmail.com>
2018-06-10 15:19:17 -0400

    Added additional functionallity

--- src/main/java/com/revature/servlet/LoginServer.java
+++ src/main/java/com/revature/servlet/LoginServer.java
@@ -1,23 +1,34 @@
 package com.revature.servlet;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import javax.servlet.http.HttpSession;
 /**
  * Servlet implementation class LoginServer
  */
 public class LoginServer extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	public static com.revature.ConnFactory cf =com.revature.ConnFactory.getInstance();
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//Create a new session cookie!
+	      HttpSession session = request.getSession(true);
+	    
+	      
+	      System.out.println("Session " + session.getId() + " just rejoined. It was made at " + session.getCreationTime() + " and last seen at " + session.getLastAccessedTime());
 		response.getWriter().append("Served by doget at LoginServer at: ").append(request.getContextPath());
 		RequestDispatcher rd = request.getRequestDispatcher("login.html");
 		rd.forward(request, response);
@@ -27,9 +38,33 @@ public class LoginServer extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("In doPost of loginserver!");
+		//System.out.println("In doPost of loginserver!");
+	      HttpSession session = request.getSession(true);
+	      System.out.println(request.toString());
+	      //Login a new User
+	      Connection conn = cf.getConnection();
+			String[] primaryKeys = new String[1];
+			primaryKeys[0] = "account_id";
+			String sql = "select * from accounts_table where username= ?";
+			
+			/*try {
+			PreparedStatement ps = conn.prepareStatement(sql, primaryKeys);
+			ps.setString(1, x);
+			ResultSet rs;
+
+				rs = ps.executeQuery();
+			if (!rs.next()) {
+				return false;
+			}
+			return rs.getBoolean(1);
+			
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			*/
 		//response.sendRedirect("home");
-		request.getRequestDispatcher("home.html").forward(request, response);
+		//request.getRequestDispatcher("home.html").forward(request, response);
 	}
 
 }
