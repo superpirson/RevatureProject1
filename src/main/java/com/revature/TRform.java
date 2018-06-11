308526f77bc0dd2d66e5112b8d07d4a92d7a4958
Alex Patton <superpirson@gmail.com>
2018-06-11 14:22:11 -0400

    made the form data pull work
    :

--- src/main/java/com/revature/TRform.java
+++ src/main/java/com/revature/TRform.java
@@ -3,16 +3,22 @@ package com.revature;
 import java.io.IOException;
 import java.sql.CallableStatement;
 import java.sql.PreparedStatement;
+import java.util.Hashtable;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
+
+import static com.revature.servlet.LoginServer.convertStreamToString;;
 
 
 
-
-
+@MultipartConfig
 public class TRform extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -24,13 +30,22 @@ public class TRform extends HttpServlet {
 
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		RequestDispatcher rd = request.getRequestDispatcher("TRform.html");
+		rd.forward(request, response);
+	      
 	}
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	      HttpSession session = request.getSession(true);
+
+		Hashtable<String, String> form= new Hashtable<String, String>();
+		for( Part p : request.getParts()) {
+			form.put(p.getName(),convertStreamToString(p.getInputStream()));
+		}
+		System.out.println("TRform just got a post for username: " + session.getAttribute("username") + " password: " +  session.getAttribute("password")  +" and data: " + form);
+	      
 
 		doGet(request, response);
 	}
