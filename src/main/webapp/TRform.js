b11983183a1bb67a77b7bf2c90cec3c6d4e91368
jbcoppola <jbcoppola@gmail.com>
2018-06-13 18:51:50 -0400

    form fields now update when edit link is followed

--- src/main/webapp/TRform.js
+++ src/main/webapp/TRform.js
@@ -4,24 +4,38 @@ var submitForm = function (url, form) {
     var formData = new FormData(document.getElementsByClassName(form)[0]);
     xhr.send(formData);
 }
-var loadLogin = function () {//window.location.href
-	var url = new URL(window.location.href);
-	var formID = url.searchParams.get("formID");
-	console.log(formID);
-	
-}
 var getForm = function (formID, func) {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', 'FormInfo?formID=' + formID);
-    xhr.onload = func;
+    xhr.onload = function() {
+    	var data = JSON.parse(xhr.response);
+    	func(data);
+    }
     xhr.send()
 }
+var loadLogin = function () {
+	var url = new URL(window.location.href);
+	var formID = url.searchParams.get("formID");
+	if (formID) {
+		getForm(formID, function(data) {
+			var form = data.forms[0]
+			document.getElementsByName("first-name")[0].value = form.FNAME;
+			document.getElementsByName("date")[0].value = form.DATE_COMPLETED;
+			document.getElementsByName("cost")[0].value = form.COST;
+			document.getElementsByName("reason-change")[0].value = form.REASONCHANGE;
+			document.getElementsByName("location")[0].value = form.LOCATION;
+			document.getElementsByName("description")[0].value = form.DESCRIPTION;
+			document.getElementsByName("last-name")[0].value = form.LNAME;
+			document.getElementsByName("grade")[0].value = form.GRADES_APPROVAL;
+			document.getElementsByName("event-type")[0].value = form.EVENT_TYPE;
+			document.getElementsByName("reason")[0].value = form.REASON_REIMBURSE;
+			document.getElementsByName("employee-approve")[0].value = form.EMPLOYEE_APPROVAL;
+			document.getElementsByName("reason-denial")[0].value = form.REASON_DENIAL;
+		});
+	}
+}
 var updateFormsTable = function () {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'FormInfo?formID=');
-    xhr.onload = function () {
-        var data = JSON.parse(xhr.response);
-        console.log(data);
+    getForm('', function(data) {
         for (var i = 0; i < data.forms.length; i++) {
         	//create the form div
             document.getElementsByClassName("js-display-table")[0].appendChild(document.createElement("div")).classList.add("form-" + (i + 1));
@@ -91,11 +105,10 @@ var updateFormsTable = function () {
             //the edit button
             var edit = document.createElement('a');
             document.getElementsByClassName("form-" + (i + 1))[0].appendChild(edit).innerHTML = "edit";
-            edit.classList.add("button")
+            edit.classList.add("button");
 
             edit.href = "TRForm?formID=" + data.forms[i].FORM_ID;
 
         }
-    }
-    xhr.send();
+    });
 }
