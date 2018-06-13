2e3682a5092eefd626bd5817f76b94581170fe21
rafaeltx <rafb.mariano@gmail.com>
2018-06-13 16:14:28 -0400

    added more FORMS geezus

--- alexs connection.sql
+++ alexs connection.sql
@@ -8,7 +8,7 @@ create table accounts_table(account_id number not null, username varchar(13) uni
 create  table forms_table(form_id number not null, fName varchar(13), lName varchar(13), grade varchar(13), date_completed date, 
     employee_approval varchar(13), benCo_approval varchar(13), dha_approval varchar(13), dsa_approval varchar(13), grades_approval varchar(13),
     form_status varchar(13), description varchar(250), location varchar(50), cost number,CONSTRAINT chk_cost check (cost>0), reason_denial varchar(250), reason_change varchar(250), 
-    reason_reimburse varchar(250),event_type varchar(13),forms blob ,primary key(form_id), submitted_by number not null,
+    reason_reimburse varchar(250),event_type varchar(13),files blob ,primary key(form_id), submitted_by number not null,
     CONSTRAINT foreign_key_constraint FOREIGN KEY (submitted_by) REFERENCES accounts_table(account_id));
     
     
@@ -44,6 +44,12 @@ insert into accounts_table values(account_id_seq.nextval, 'admin', 'admin', 'adm
 
 insert into forms_table values(90, 'Super-Joe','Coppola', 'A*(A+)', date '05-09-18','Yes', 'Yes', 'Yes','Yes', 'Yes',
     'pending', 'revature', 'tampa', 250, null, null, 'tranining','bootcamp',null ,103);
+    
+insert into forms_table values(93, 'Goku','Supersayan', 'B*(B+)', date '01-03-18','No', 'Yes', 'No','Yes', 'Yes',
+    'approved', 'dragon ball z', 'new york', 300, null, null, 'tranining','bootcamp',null ,106);
+    
+insert into forms_table values(96, 'Matt','Knighten', 'F*(F)', date '06-06-06','No', 'Yes', 'No','Yes', 'Yes',
+    'approved', 'real marine', 'new york', 350, null, null, 'tranining','bootcamp',null ,109);
 
 
 
