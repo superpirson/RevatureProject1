13f1622e106e5aed0b44e99b1764c5f2498e8ca1
jbcoppola <jbcoppola@gmail.com>
2018-06-12 20:15:22 -0400

    got fetch displaying

--- src/main/webapp/TRform.js
+++ src/main/webapp/TRform.js
@@ -12,3 +12,25 @@ var submitLogin = function () {
     var formData = new FormData(document.getElementsByClassName("login-form")[0]);
     xhr.send(formData);
 }
+
+var getForm = function () {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'FormInfo?formID=90');
+    xhr.onload = function () {
+        var data = JSON.parse(xhr.response);
+        console.log(data);
+        for (var i = 0; i < data.forms.length; i++) {
+            document.getElementsByClassName("js-display-table")[0].appendChild(document.createElement("div")).classList.add("form-" + (i + 1));
+            for (var prop in data.forms[i]) {
+            	if (data.forms[i][prop]) {
+            	//create the prop div
+            	document.getElementsByClassName("form-" + (i + 1))[0].appendChild(document.createElement("div")).classList.add(prop);
+            	
+            	//fill in the data
+            	document.querySelector("." + [prop]).innerHTML = [prop] + " " + data.forms[i][prop];
+            	}
+            }
+        }
+    }
+    xhr.send();
+}
